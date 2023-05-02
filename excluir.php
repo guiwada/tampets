b88a8d96a366866957b26e38a971437271050d8a
Guilherme Wada <wadas3241@gmail.com>
2023-05-01 23:51:35 -0300

    mudanca na funçao excluir

--- excluir.php
+++ excluir.php
@@ -3,15 +3,16 @@ $servidor="localhost";
 $usuario="root";
 $senha="";
 $dbname = "upx";
-$conn = mysqli_connect($servidor,$usuario,$senha,$dbname);$id = $_GET['id'];
+
+$id = $_GET['id'];
+$conn = mysqli_connect($servidor,$usuario,$senha,$dbname);
 
 $sql = "DELETE FROM usuario WHERE id = $id";
 if (mysqli_query($conn, $sql)) {
     echo "Usuário excluído com sucesso. <br>";
     echo "<a href='listar.php'>Voltar para a lista de usuários</a>";
+    mysqli_close($conn);
 } else {
     echo "Erro ao excluir usuário: " . mysqli_error($conn);
 }
-
-mysqli_close($conn);
 ?>
